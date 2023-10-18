e5ba84ab3c15bd728c8a17d7ba50ed4b0315e378
Itamar Turner-Trauring <itamar@pythonspeed.com>
2023-10-18 16:28:10 -0400

    Switch to floating point so we can use perf stat to validate use of SIMD

--- benchmark.py
+++ benchmark.py
@@ -1,15 +1,14 @@
-import numpy as np
 from time import time
+import numpy as np
+from simd_example import average_arrays
 
 # Two random arrays of unsigned integers
-ARR1 = np.random.randint(0, 1_000, (1_000_000), dtype=np.uint16)
-ARR2 = np.random.randint(0, 1_000, (1_000_000), dtype=np.uint16)
-
-from simd_example import add_arrays
+ARR1 = np.random.random((1_000_000,)).astype(np.float32)
+ARR2 = np.random.random((1_000_000,)).astype(np.float32)
 
 start = time()
 for i in range(10_000):
-    add_arrays(ARR1, ARR2)
+    average_arrays(ARR1, ARR2)
 elapsed = (time() - start) / 10_000
 
 print("Time per call (milliseconds):", elapsed * 1000)
--- src/simd_example/_extension.pyx
+++ src/simd_example/_extension.pyx
@@ -2,14 +2,13 @@
 
 import numpy as np
 cimport numpy as cnp
-from libc.stdint cimport uint16_t
 cimport cython
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def add_arrays(uint16_t[::1] arr1, uint16_t[::1] arr2):
-    out = np.empty((len(arr1), ), dtype=np.uint16)
-    cdef uint16_t[::1] out_view = out
+def average_arrays(cnp.float32_t[::1] arr1, cnp.float32_t[::1] arr2):
+    out = np.empty((len(arr1), ), dtype=np.float32)
+    cdef cnp.float32_t[::1] out_view = out
     for i in range(len(arr1)):
-        out_view[i] = 5 + (arr1[i] + arr2[i]) >> 2
+        out_view[i] = (arr1[i] + arr2[i]) / 2
     return out
