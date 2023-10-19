042f3ba61a3f2345d9a175ca4ecf00534a982c80
Itamar Turner-Trauring <itamar@pythonspeed.com>
2023-10-19 16:38:29 -0400

    Update

--- src/simd_example/_extension.pyx
+++ src/simd_example/_extension.pyx
@@ -14,9 +14,9 @@ def average_arrays(cnp.float32_t[::1] arr1, cnp.float32_t[::1] arr2):
     return out
 
 
-def slow_average_arrays(cnp.float32_t[:] arr1, cnp.float32_t[:] arr2):
-    out = np.empty(arr1.shape, dtype=np.float32)
-    cdef cnp.float32_t[:] out_view = out.ravel()
+def average_arrays_1(cnp.float32_t[:] arr1, cnp.float32_t[:] arr2):
+    out = np.empty((len(arr1), ), dtype=np.float32)
+    cdef cnp.float32_t[:] out_view = out
     for i in range(len(arr1)):
         out_view[i] = (arr1[i] + arr2[i]) / 2
     return out
