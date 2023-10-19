7abe19ddf1109637384dad89328c0670bb837082
Itamar Turner-Trauring <itamar@pythonspeed.com>
2023-10-19 12:18:08 -0400

    Nicer output

--- benchmark.py
+++ benchmark.py
@@ -7,8 +7,8 @@ ARR1 = np.random.random((1_000_000,)).astype(np.float32)
 ARR2 = np.random.random((1_000_000,)).astype(np.float32)
 
 start = time()
-for i in range(10_000):
+for i in range(1_000):
     average_arrays(ARR1, ARR2)
-elapsed = (time() - start) / 10_000
+elapsed = (time() - start) / 1_000
 
-print("Time per call (milliseconds):", elapsed * 1000)
+print(f"Time per call: {elapsed * 1000:.2} ms")
