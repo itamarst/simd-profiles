146868354ae773e97b6bf9f98d54d225b4886520
Itamar Turner-Trauring <itamar@pythonspeed.com>
2023-10-19 14:31:22 -0400

    Linear and strided

--- benchmark.py
+++ benchmark.py
@@ -2,13 +2,20 @@ from time import time
 import numpy as np
 from simd_example import average_arrays
 
-# Two random arrays of unsigned integers
-ARR1 = np.random.random((1_000_000,)).astype(np.float32)
-ARR2 = np.random.random((1_000_000,)).astype(np.float32)
 
-start = time()
-for i in range(1_000):
-    average_arrays(ARR1, ARR2)
-elapsed = (time() - start) / 1_000
+def timeit(title, arr1, arr2):
+    start = time()
+    for i in range(1_000):
+        out = average_arrays(arr1, arr2)
+    elapsed = (time() - start) / 1_000
 
-print(f"Time per call: {elapsed * 1000:.2} ms")
+    print(f"Time per call, {title}: {elapsed * 1000:.2} ms")
+
+
+ARR = np.random.random((1_000_000)).astype(np.float32)
+ARR2 = np.random.random((1_000_000)).astype(np.float32)
+timeit("linear", ARR, ARR2)
+
+ARR3 = np.random.random((16_000_000,)).astype(np.float32)
+ARR4 = np.random.random((16_000_000,)).astype(np.float32)
+timeit("strided", ARR3[::16], ARR4[::16])
--- src/simd_example/_extension.pyx
+++ src/simd_example/_extension.pyx
@@ -7,8 +7,16 @@ cimport cython
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def average_arrays(cnp.float32_t[::1] arr1, cnp.float32_t[::1] arr2):
-    out = np.empty((len(arr1), ), dtype=np.float32)
+    out = np.empty((len(arr1),), dtype=np.float32)
     cdef cnp.float32_t[::1] out_view = out
+    for i in range(len(arr1)):
+        out_view[i] = (arr1[i] + arr2[i]) / 2
+    return out
+
+
+def slow_average_arrays(cnp.float32_t[:] arr1, cnp.float32_t[:] arr2):
+    out = np.empty(arr1.shape, dtype=np.float32)
+    cdef cnp.float32_t[:] out_view = out.ravel()
     for i in range(len(arr1)):
         out_view[i] = (arr1[i] + arr2[i]) / 2
     return out
