ef4f5f9180ca868cf473594c5400f839f0f62ea7
Itamar Turner-Trauring <itamar@pythonspeed.com>
2023-10-20 10:21:17 -0400

    Work all the way to fast version

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,11 @@
 from time import time
 import numpy as np
-from simd_example import average_arrays
+import sys
+import simd_example
+
+# Use the first command-line argument to choose which
+# function to call:
+average_arrays = getattr(simd_example, sys.argv[1])
 
 
 def timeit(title, arr1, arr2):
@@ -12,10 +17,12 @@ def timeit(title, arr1, arr2):
     print(f"Time per call, {title}: {elapsed * 1000:.2} ms")
 
 
-ARR = np.random.random((1_000_000)).astype(np.float32)
-ARR2 = np.random.random((1_000_000)).astype(np.float32)
-timeit("linear", ARR, ARR2)
+# Arrays laid out linearly in memory:
+ARR1 = np.random.random((1_000_000,)).astype(np.float32)
+ARR2 = np.random.random((1_000_000,)).astype(np.float32)
+timeit("contiguous", ARR1, ARR2)
 
+# Arrays where we grab every 16th item:
 ARR3 = np.random.random((16_000_000,)).astype(np.float32)
 ARR4 = np.random.random((16_000_000,)).astype(np.float32)
 timeit("strided", ARR3[::16], ARR4[::16])
--- src/simd_example/_extension.pyx
+++ src/simd_example/_extension.pyx
@@ -4,15 +4,6 @@ import numpy as np
 cimport numpy as cnp
 cimport cython
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-def average_arrays(cnp.float32_t[::1] arr1, cnp.float32_t[::1] arr2):
-    out = np.empty((len(arr1),), dtype=np.float32)
-    cdef cnp.float32_t[::1] out_view = out
-    for i in range(len(arr1)):
-        out_view[i] = (arr1[i] + arr2[i]) / 2
-    return out
-
 
 def average_arrays_1(cnp.float32_t[:] arr1, cnp.float32_t[:] arr2):
     out = np.empty((len(arr1), ), dtype=np.float32)
@@ -20,3 +11,50 @@ def average_arrays_1(cnp.float32_t[:] arr1, cnp.float32_t[:] arr2):
     for i in range(len(arr1)):
         out_view[i] = (arr1[i] + arr2[i]) / 2
     return out
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def average_arrays_2(cnp.float32_t[:] arr1, cnp.float32_t[:] arr2):
+    if len(arr1) != len(arr2):
+        raise ValueError("Arrays must be the same size")
+    out = np.empty((len(arr1), ), dtype=np.float32)
+    cdef cnp.float32_t[:] out_view = out
+    for i in range(len(arr1)):
+        out_view[i] = (arr1[i] + arr2[i]) / 2
+    return out
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def average_arrays_3(cnp.float32_t[::1] arr1, cnp.float32_t[::1] arr2):
+    if len(arr1) != len(arr2):
+        raise ValueError("Arrays must be the same size")
+    out = np.empty((len(arr1), ), dtype=np.float32)
+    cdef cnp.float32_t[::1] out_view = out
+    for i in range(len(arr1)):
+        out_view[i] = (arr1[i] + arr2[i]) / 2
+    return out
+
+
+# This type will be one of these two underlying types at compile time:
+ctypedef fused float_arr_t:
+   cnp.float32_t[:]
+   cnp.float32_t[::1]
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef average_arrays_impl(float_arr_t arr1, float_arr_t arr2):
+    out = np.empty((len(arr1), ), dtype=np.float32)
+    cdef cnp.float32_t[::1] out_view = out
+    for i in range(len(arr1)):
+        out_view[i] = (arr1[i] + arr2[i]) / 2
+    return out
+
+def average_arrays_4(arr1, arr2):
+    if len(arr1) != len(arr2):
+        raise ValueError("Arrays must be the same size")
+    if arr1.data.contiguous and arr2.data.contiguous:
+        return average_arrays_impl[cnp.float32_t[::1]](arr1, arr2)
+    else:
+        return average_arrays_impl[cnp.float32_t[:]](arr1, arr2)
